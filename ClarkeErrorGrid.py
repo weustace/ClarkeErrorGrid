0662c4605d8943304377c1a0f6de4cc7faa7615e
William Eustace <weustace@wje.io>
2020-05-23 15:54:58 +0100

    Added kwargs for plot and removed redundant title string

--- ClarkeErrorGrid.py
+++ ClarkeErrorGrid.py
@@ -1,91 +1,9 @@
-'''
-CLARKE ERROR GRID ANALYSIS      ClarkeErrorGrid.py
-
-Need Matplotlib Pyplot
-
-
-The Clarke Error Grid shows the differences between a blood glucose predictive measurement and a reference measurement,
-and it shows the clinical significance of the differences between these values.
-The x-axis corresponds to the reference value and the y-axis corresponds to the prediction.
-The diagonal line shows the prediction value is the exact same as the reference value.
-This grid is split into five zones. Zone A is defined as clinical accuracy while
-zones C, D, and E are considered clinical error.
-
-Zone A: Clinically Accurate
-    This zone holds the values that differ from the reference values no more than 20 percent
-    or the values in the hypoglycemic range (<70 mg/dl).
-    According to the literature, values in zone A are considered clinically accurate.
-    These values would lead to clinically correct treatment decisions.
-
-Zone B: Clinically Acceptable
-    This zone holds values that differe more than 20 percent but would lead to
-    benign or no treatment based on assumptions.
-
-Zone C: Overcorrecting
-    This zone leads to overcorrecting acceptable BG levels.
-
-Zone D: Failure to Detect
-    This zone leads to failure to detect and treat errors in BG levels.
-    The actual BG levels are outside of the acceptable levels while the predictions
-    lie within the acceptable range
-
-Zone E: Erroneous treatment
-    This zone leads to erroneous treatment because prediction values are opposite to
-    actual BG levels, and treatment would be opposite to what is recommended.
-
-
-SYNTAX:
-        plot, zone = clarke_error_grid(ref_values, pred_values, title_string)
-
-INPUT:
-        ref_values          List of n reference values.
-        pred_values         List of n prediciton values.
-        title_string        String of the title.
-
-OUTPUT:
-        plot                The Clarke Error Grid Plot returned by the function.
-                            Use this with plot.show()
-        zone                List of values in each zone.
-                            0=A, 1=B, 2=C, 3=D, 4=E
-
-EXAMPLE:
-        plot, zone = clarke_error_grid(ref_values, pred_values, "00897741 Linear Regression")
-        plot.show()
-
-References:
-[1]     Clarke, WL. (2005). "The Original Clarke Error Grid Analysis (EGA)."
-        Diabetes Technology and Therapeutics 7(5), pp. 776-779.
-[2]     Maran, A. et al. (2002). "Continuous Subcutaneous Glucose Monitoring in Diabetic
-        Patients" Diabetes Care, 25(2).
-[3]     Kovatchev, B.P. et al. (2004). "Evaluating the Accuracy of Continuous Glucose-
-        Monitoring Sensors" Diabetes Care, 27(8).
-[4]     Guevara, E. and Gonzalez, F. J. (2008). Prediction of Glucose Concentration by
-        Impedance Phase Measurements, in MEDICAL PHYSICS: Tenth Mexican
-        Symposium on Medical Physics, Mexico City, Mexico, vol. 1032, pp.
-        259261.
-[5]     Guevara, E. and Gonzalez, F. J. (2010). Joint optical-electrical technique for
-        noninvasive glucose monitoring, REVISTA MEXICANA DE FISICA, vol. 56,
-        no. 5, pp. 430434.
-
-
-Made by:
-Trevor Tsue
-7/18/17
-
-Based on the Matlab Clarke Error Grid Analysis File Version 1.2 by:
-Edgar Guevara Codina
-codina@REMOVETHIScactus.iico.uaslp.mx
-March 29 2013
-'''
-
-
-
 import matplotlib.pyplot as plt
 
 
 #This function takes in the reference values and the prediction values as lists and returns a list with each index corresponding to the total number
 #of points within that zone (0=A, 1=B, 2=C, 3=D, 4=E) and the plot
-def clarke_error_grid(ref_values, pred_values, title_string):
+def clarke_error_grid(ref_values, pred_values, **plt_args):
 
     #Checking to see if the lengths of the reference and prediction arrays are the same
     assert (len(ref_values) == len(pred_values)), "Unequal number of values (reference : {}) (prediction : {}).".format(len(ref_values), len(pred_values))
@@ -100,8 +18,7 @@ def clarke_error_grid(ref_values, pred_values, title_string):
     plt.clf()
 
     #Set up plot
-    plt.scatter(ref_values, pred_values, marker='o', color='black', s=8)
-    plt.title(title_string + " Clarke Error Grid")
+    plt.scatter(ref_values, pred_values,**plt_args)
     plt.xlabel("Reference Concentration (mg/dl)")
     plt.ylabel("Prediction Concentration (mg/dl)")
     plt.xticks([0, 50, 100, 150, 200, 250, 300, 350, 400])
--- examples/plot_errors.py
+++ examples/plot_errors.py
@@ -10,6 +10,7 @@ actual_concs = np.random.rand(100)*400
 errors = (np.random.random(100)*0.5)+0.75 #need to centre at 1
 pred_concs = actual_concs * errors
 improved_pred_concs = actual_concs * ((np.random.random(100)*0.1)+0.9)
-plot_obj, zones = clarke_error_grid(actual_concs,pred_concs,"")
-plot_obj.scatter(actual_concs,improved_pred_concs,color='green',marker='x')
+plot_obj, zones = clarke_error_grid(actual_concs,pred_concs,color="red",marker='.',label="Original prediction method")
+plot_obj.scatter(actual_concs,improved_pred_concs,color='green',marker='x',label="New and improved method!")
+plt.legend()
 plt.show()
